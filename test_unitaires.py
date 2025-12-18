4832d6e1da2d3c17ba4454c7b4f96c37ecede4c6
EliseeNR <elisee.noirot-rathar@efrei.net>
2025-12-18 12:12:08 +0100

    test unitaires avec pytest

---
+++ test_unitaires.py
@@ -0,0 +1,23 @@
+import pytest
+import joblib
+
+
+# Charger le modèle
+def load_model():
+    return joblib.load('regression.joblib')
+
+@pytest.fixture
+def model():
+    return load_model()
+
+def test_prediction_positive(model):
+    """Vérifie que la prédiction renvoie un nombre positif"""
+    X = [[100, 3, 1]]  # taille=100, 3 chambres, jardin=1
+    prediction = model.predict(X)
+    assert prediction[0] > 0
+
+def test_prediction_type(model):
+    """Vérifie que la prédiction est bien un float"""
+    X = [[80, 2, 0]]
+    prediction = model.predict(X)
+    assert isinstance(prediction[0], float)
